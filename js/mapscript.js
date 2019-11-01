5ae4cdaea442c0a854fa4ab73e9a8938f48df98e
vanmeciv <vanmeciv@gmail.com>
2019-11-01 16:39:17 -0700

    Add files via upload

--- js/mapscript.js
+++ js/mapscript.js
@@ -17,11 +17,6 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 		// add GeoJSON layer to the map once the file is loaded
 		L.geoJson(data).addTo(mymap);
 	});
-// load GeoJSON from an external file http://shpescape.com/mix/uploads/9e9490fc30b6045dc0d003db2680f381.json/ - converted on http://shpescape.com/mix/
-	$.getJSON("geoJSON/laharEvacRoutes.json",function(data){
-		// add GeoJSON layer to the map once the file is loaded
-		L.geoJson(data).addTo(mymap);
-	});
 // load GeoJSON from an external file http://shpescape.com/mix/uploads/56c45ffcc7ab7606844b95e0d3579920.json/ - converted on http://shpescape.com/mix/
 	$.getJSON("geoJSON/parks.json",function(data){
 		// add GeoJSON layer to the map once the file is loaded
