d394b9d3420d31887dda0ffd818c7cdc93e0f1b1
vanmeciv <vanmeciv@gmail.com>
2019-11-15 15:47:09 -0800

    Add files via upload

--- js/lab_3.js
+++ js/lab_3.js
@@ -4,7 +4,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	id: 'mapbox.streets',
-	accessToken: 'pk.eyJ1IjoiaXNhYWN2IiwiYSI6ImNrMnpqYnVxaTA1b3IzbXBnaG5zY3o3eTEifQ.kMdIcXYBFKHTorj3Hxgi7g'
+	accessToken: 'pk.eyJ1IjoiaXNhYWN2IiwiYSI6ImNrMzBxc3l3cDAxOWEzbXJ6NnNtYjJtM2QifQ.hMUMXLCYuJ8i01uCnvWn6g'
 }).addTo(mymap);
 
 
