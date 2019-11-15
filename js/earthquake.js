ddf470c5bac5212347a6c1acf927ee711461a31d
vanmeciv <vanmeciv@gmail.com>
2019-11-15 14:28:25 -0800

    Add files via upload

--- js/earthquake.js
+++ js/earthquake.js
@@ -1,23 +1,25 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiaXNhYWN2IiwiYSI6ImNrMmdqMmVpMDBnYmczYnBoaHhzbDJybGgifQ.5_1sNMFnGpLHtF5fv_Yxuw';
+mapboxgl.accessToken = 'pk.eyJ1IjoiaXNhYWN2IiwiYSI6ImNrMnpqYnVxaTA1b3IzbXBnaG5zY3o3eTEifQ.kMdIcXYBFKHTorj3Hxgi7g';
 var map = new mapboxgl.Map({
 container: 'map',
-style: 'mapbox://styles/mapbox/streets-v11'
+style: 'mapbox://styles/isaacv/ck2wpjhhk0nr31dmrnv8v1p9a'
 });
 
 
-// var url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson';
-// map.on('load', function () {
-// 	window.setInterval(function() {
-// 	mapbox.getSource('drone').setData(url);
-// 	}, 2000);
-//
-// 	map.addSource('drone', { type: 'geojson', data: url });
-// 	map.addLayer({
-// 		"id": "drone",
-// 		"type": "symbol",
-// 		"source": "drone",
-// 		"layout": {
-// 	  "icon-image": "rocket-15"
-// 	}
-// 	});
-// });
+
+
+var url = 'https://wanderdrone.appspot.com/';
+map.on('load', function () {
+window.setInterval(function() {
+map.getSource('drone').setData(url);
+}, 2000);
+
+map.addSource('drone', { type: 'geojson', data: url });
+map.addLayer({
+"id": "drone",
+"type": "symbol",
+"source": "drone",
+"layout": {
+"icon-image": "rocket-15"
+}
+});
+});
