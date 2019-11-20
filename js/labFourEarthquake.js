ba6abdd262da353ab810cf9911e2dd055950ba8e
vanmeciv <vanmeciv@gmail.com>
2019-11-19 17:05:07 -0800

    Add files via upload

--- js/labFourEarthquake.js
+++ js/labFourEarthquake.js
@@ -1,11 +1,14 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiaXNhYWN2IiwiYSI6ImNrMnpqYnVxaTA1b3IzbXBnaG5zY3o3eTEifQ.kMdIcXYBFKHTorj3Hxgi7g';
 var map = new mapboxgl.Map({
 container: 'labFourEarthquake',
+center: [-122.4443, 47.2529],
+zoom: 1,
+attributionControl: false,
 style: 'mapbox://styles/isaacv/ck2wpjhhk0nr31dmrnv8v1p9a'
 });
+map.addControl(new mapboxgl.AttributionControl(), 'top-right');
 
-
-var url = 'https://wanderdrone.appspot.com/';
+var url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson';
 map.on('load', function () {
 window.setInterval(function() {
 map.getSource('drone').setData(url);
