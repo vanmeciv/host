faa8006892fb35d63f4e8ebba04c83dc27705a55
Isaac V <vanmeciv@gmail.com>
2019-12-07 12:56:53 -0800

    Update cats.js
    checking cors policy

--- js/cats.js
+++ js/cats.js
@@ -1,57 +1,114 @@
+// mapboxgl.accessToken = 'pk.eyJ1IjoiaXNhYWN2IiwiYSI6ImNrMnpqYnVxaTA1b3IzbXBnaG5zY3o3eTEifQ.kMdIcXYBFKHTorj3Hxgi7g';
+//
+// var map = new mapboxgl.Map({
+//     container: 'map',
+//     style: 'mapbox://styles/mapbox/streets-v11',
+//     center: [-65.017, -16.457],
+//     zoom: 5
+// });
+//
+// map.on('load', function(){
+//   map.addSource('units', {
+//     type: 'geojson',
+//     data: 'geoJSON/cats.json'
+//     });
+//
+//   var geojson = map.addLayer({
+//     id: 'units',
+//     type: 'symbol',
+//     source: 'unit'
+//   });
+// });
+//
+// // add markers to map
+// geojson.features.forEach(function(marker) {
+//     // create a DOM element for the marker
+//     var el = document.createElement('div');
+//     el.className = 'marker';
+//     el.style.backgroundImage = 'url(https://vanmeciv.github.io/host/img/icons/' + marker.properties.SymbolID  '.png)';
+//
+//
+//     el.addEventListener('click', function() {
+//         window.alert(marker.properties.message);
+//     });
+//
+//     // add marker to map
+//     new mapboxgl.Marker(el)
+//         .setLngLat(marker.geometry.coordinates)
+//         .addTo(map);
+// });
+
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiaXNhYWN2IiwiYSI6ImNrMnpqYnVxaTA1b3IzbXBnaG5zY3o3eTEifQ.kMdIcXYBFKHTorj3Hxgi7g';
-var geojson = {
-    "type": "FeatureCollection",
-    "features": [
-        {
-            "type": "Feature",
-            "properties": {
-                "message": "Foo",
-                "SymbolID": [0]
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -66.324462890625,
-                    -16.024695711685304
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "message": "Bar",
-                "SymbolID": [1]
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -61.2158203125,
-                    -15.97189158092897
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "message": "Baz",
-                "SymbolID": [2]
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -63.29223632812499,
-                    -18.28151823530889
-                ]
-            }
-        }
-    ]
-};
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
     center: [-65.017, -16.457],
     zoom: 5
 });
+
+map.on('load', function(){
+  map.addSource('units', {
+    type: 'geojson',
+    data: 'geoJSON/cats.json'
+    });
+
+  var geojson = map.addLayer({
+    id: 'units',
+    type: 'symbol',
+    source: 'units'
+  });
+});
+
+// var geojson = {
+//
+//     "type": "FeatureCollection",
+//     "features": [
+//         {
+//             "type": "Feature",
+//             "properties": {
+//                 "message": "Foo",
+//                 "SymbolID": [0]
+//             },
+//             "geometry": {
+//                 "type": "Point",
+//                 "coordinates": [
+//                     -66.324462890625,
+//                     -16.024695711685304
+//                 ]
+//             }
+//         },
+//         {
+//             "type": "Feature",
+//             "properties": {
+//                 "message": "Bar",
+//                 "SymbolID": [1]
+//             },
+//             "geometry": {
+//                 "type": "Point",
+//                 "coordinates": [
+//                     -61.2158203125,
+//                     -15.97189158092897
+//                 ]
+//             }
+//         },
+//         {
+//             "type": "Feature",
+//             "properties": {
+//                 "message": "Baz",
+//                 "SymbolID": [2]
+//             },
+//             "geometry": {
+//                 "type": "Point",
+//                 "coordinates": [
+//                     -63.29223632812499,
+//                     -18.28151823530889
+//                 ]
+//             }
+//         }
+//     ]
+// };
+
 // add markers to map
 geojson.features.forEach(function(marker) {
     // create a DOM element for the marker
