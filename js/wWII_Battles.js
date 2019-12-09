7e4e003735d0a96e2df704062c52adae05412457
Isaac V <vanmeciv@gmail.com>
2019-12-09 13:53:49 -0800

    testing url string...still

--- js/wWII_Battles.js
+++ js/wWII_Battles.js
@@ -48,7 +48,10 @@ battlesMap.on('click', 'wWII_Battles', function (e) {
       var year = e.features[0].properties.name.slice(-4);
       var brief = e.features[0].properties.description.slice(0,50);
       var myString = e.features[0].properties.description
-      var myurl = myString.substring(myString.indexOf("Wikipedia) (")+1)
+
+      var myurl = myString.substring(myString.indexOf("Wikipedia) (")+25)
+      var url = myString.substring(myString.indexOf(') (')+1)
+
 
       var description = "<h4>Battle Name: </h4>" + "<br>" + e.features[0].properties.name + "<br>Description: " + brief + "..." + "<br>More Details: " + "<a target='_blank' href=" + myurl + ">Click Here</a>";
       //3. make the popup
