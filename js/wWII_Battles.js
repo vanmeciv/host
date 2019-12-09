398c52c799361ce40f294e18179d65d8f13e2cef
Isaac V <vanmeciv@gmail.com>
2019-12-09 14:18:52 -0800

    starting over tests of url string

--- js/wWII_Battles.js
+++ js/wWII_Battles.js
@@ -47,14 +47,10 @@ battlesMap.on('click', 'wWII_Battles', function (e) {
       // var description = e.features[0].properties.title;
       var year = e.features[0].properties.name.slice(-4);
       var brief = e.features[0].properties.description.slice(0,50);
-      var myString = e.features[0].properties.description
-
-      var myurl = myString.substring(myString.indexOf("(Source:"),-1)
-
-      // var url = myString.substring(myString.indexOf(') (')+1)
-
-
-      var description = "<h4>Battle Name: </h4>" + "<br>" + e.features[0].properties.name + "<br>Description: " + brief + "..." + "<br>More Details: " + "<a target='_blank' href=" + myurl + ">Click Here</a>";
+      var myString= e.features[0].properties.description
+      myString = myString.substring(myString.indexOf(') (')+1)
+      
+      var description = "<h4>Battle Name: </h4>" + "<br>" + e.features[0].properties.name + "<br>Description: " + brief + "..." + "<br>More Details: " + "<a target='_blank' href=" + e.features[0].properties.url + ">Click Here</a>";
       //3. make the popup
       new mapboxgl.Popup()
               .setLngLat(coordinates)
